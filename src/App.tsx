56398412a328ef9274ea6d7c310e24a4df25e81a
Justin Pham <justin@nora.lol>
2021-01-19 17:00:20 -0800

    Added Evan's code for phone frame.  Phone frame isn't loading

--- src/App.tsx
+++ src/App.tsx
@@ -5,33 +5,40 @@ import Phone from "./Components/Phone";
 import Routes from "./Routes";
 
 const StyledRoot = styled.div`
-  background-color: #272C35;
+  background-color: #272c35;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
+const StyledDiv = styled.div``;
 
 export default function App() {
   const [squish, setSquish] = useState(false);
   const [squished, setSquished] = useState(false);
-  const renderStart = ()=> setSquish(true);
+  const renderStart = () => setSquish(true);
   const [phone, setPhone] = useState("");
   useEffect(() => {
     if (window.innerWidth > 420) {
-    if (window.innerHeight < 903) {
-      setPhone("block")
-      setSquish(true)
-    } else {
-      setPhone("block")
+      if (window.innerHeight < 903) {
+        setPhone("block");
+        setSquish(true);
+      } else {
+        setPhone("block");
+      }
     }
-  }
-  }, [])
+  }, []);
   return (
-    <Phone trigger={renderStart} squish={squish}>
-      <StyledRoot>
-      <Routes />
-    </StyledRoot>
-    </Phone>
+    <StyledDiv className="App">
+      <div style={{ display: phone }} id="phone">
+        <div id="speaker"></div>
+        <div id="camera"></div>
+      </div>
+      <Phone trigger={renderStart} squish={squish}>
+        <StyledRoot>
+          <Routes />
+        </StyledRoot>
+      </Phone>
+    </StyledDiv>
   );
 }
